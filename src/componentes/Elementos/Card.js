a83d33ad8823c0db2ea19f3fd052121fe9e9a0bb
Igor Khalil <igorfros@gmail.com>
2024-02-23 12:35:35 -0300

    Implementado o novo sistema de estado

--- src/componentes/Elementos/Card.js
+++ src/componentes/Elementos/Card.js
@@ -1,18 +1,32 @@
 import styles from "./Card.module.css"
 import ButtonB from "../Elementos/ButtonB"
+import { useState } from "react"
 
 function Card({img,title,tech,description,repo,site}){
+
+    const [info, setInfo] = useState(false)
+
+    function InfoOn(){
+        setInfo(true)
+    }
+    function InfoOff(){
+        setInfo(false)
+    }
+
     return(
-        <div className={styles.card}>
-            <a href={site}>
+        <div onMouseLeave={InfoOff} className={styles.card}>
+            <a onMouseEnter={InfoOn} href={site}>
                 <img src ={img} alt="ERRO"/>
             </a>
-            <section>
-                <h3>{title}</h3>
-                <p> <strong>Tecnologias:</strong>{tech}</p>
-                <p> {description}</p>
-                <ButtonB text="Acesse o repositório" link={repo}/>
-            </section>
+
+            {info === true &&(
+                <section>
+                    <h3>{title}</h3>
+                    <p> <strong>Tecnologias:</strong>{tech}</p>
+                    <p> {description}</p>
+                    <ButtonB text="Acesse o repositório" link={repo}/>
+                </section>
+            )}  
         </div>
     )
 }
--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,11 +1,47 @@
 import styles from "./Presentation.module.css"
 import ButtonA from "../Elementos/ButtonA"
+import {useState, useEffect} from "react"
 
 function Presentation(){
+
+    const [text, setText] = useState('');
+    const toRotate = ["Igor Khalil", 'Desenvolvedor Front End'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const[delta, setDelta] = useState(100)
+
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        },delta)
+        return ()=>{clearInterval(ticker)}
+    }, [text])
+
+    const toType = () =>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i];
+        let updateText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+        
+        setText(updateText);
+
+        if(!isDeleting && updateText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if (isDeleting && updateText === ""){
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+        }
+    }
+
+
+
     return(
         <div id="Presentation" className={styles.presentation}>
             <h4><strong>Bem vindo ao meu portifólio</strong></h4>
-            <h1>Olá, eu sou o Igor Khalil</h1>
+            <h1>Olá, eu sou o {text}</h1>
             <p> 
                 Sou técnico em Eletrônica e em Engenharia Clínica e atualmente estou fazendo minha graduação em Ciências da Computação pela UFMA.
                 <br/>Minhas experiências no mercado de trabalho me permitiram absolver muitas habilidades como, trabalho em grupo, proatividade, busca de conhecimento,
